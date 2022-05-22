239c6df6ab76a4ad8809e0b544eb3c95c4bf445f
PedroVitor-oss <pedro.santos22@escola.pr.edu.br>
2022-05-21 21:07:58 -0300

    primeiro Repositorio

--- App.js
+++ App.js
@@ -1,11 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState}  from 'react';
+import { StyleSheet, Text, View} from 'react-native';
+import {Picker} from '@react-native-picker/picker';
 
 export default function App() {
+  const getCurrentDate=()=>{
+    var date = String(new Date().getDate());
+    var month = String(new Date().getMonth() + 1);
+    var year = new Date().getFullYear();
+
+    if(month.length == 1)
+    month = '0'+month
+    if(date.length == 1)
+    date = '0'+date
+
+
+    return date + '/' + month + '/' + year;
+  }
+  const [selectedClient, setSelectedClinnet] = useState();
+  
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.header}>
+      <Picker
+      style={styles.selectorClients}
+    selectedValue={selectedClient}
+   onValueChange={(itemValue, itemIndex) =>setSelectedClinnet(itemValue)}>
+  <Picker.Item label="Ariane" value="Ariane" />
+  <Picker.Item label="Sandra" value="Sandra" />
+   <Picker.Item label="Gessica" value="Gessica" />
+  <Picker.Item label="Susana" value="Susana" />
+</Picker>
+
+        <Text style={styles.data}>{getCurrentDate()}</Text>
+      </View>
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -13,9 +43,29 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 0,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header:{
+    width:320,
+    height:100,
+    backgroundColor:'#BEBEBE',
+    padding:10,
+    paddingTop:35,
+  },
+  data:{
+    position:'absolute',
+    right:10,
+    top:42,
+    fontSize:20,
+  },
+  selectorClients:{
+    width:150,
+    height:20,
+    
+
+  },
+ 
 });
